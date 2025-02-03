1b3f3add7b7f635a5bd999fc7a0177a7160a19b5
Jairo Gordillo <gordillorojasjairo9@gmail.com>
2025-02-03 11:50:54 -0500

    Apellido

--- ejemplo.py
+++ ejemplo.py
@@ -1,10 +1,10 @@
 palabra="CHAO"
 print(palabra)
-nombre=input("Ingrese su nombre: ")
-print("Hola",nombre)
+nombre=input("Ingrese su apellido: ")
+print("Buenos días señor(a) ",nombre)
 
 for i in range(5):
-    print(i)
+    print("bof"*i)
 
 color=input("Ingrese un color: ")
 print("Tu color favorito es",color)
